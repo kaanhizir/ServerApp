a07fcd22e36eabdac908dc31670b5ea2998c2834
kaanhizir <kaanhizir53@gmail.com>
2023-09-13 17:39:10 +0300

    main

--- ServerApp.js
+++ ServerApp.js
@@ -13,6 +13,24 @@ app.get("/",(req,res,next)=>{
     res.send("MainPage")
 })
 
+//Ekleme İşleme
+
+app.get("/add", (req,res,next)=>{
+    res.send("Ekleme İşlemi Burada Gerçekleşir.")
+})
+
+//Silme İşlemi
+
+app.get("/delete",(req,res,next)=>{
+    res.send("Silme İşlemi Burada Gerçekleşir.")
+})
+
+//Güncelleme İşlemi
+
+app.get("/update",(req,res,next)=>{
+    res.send("Güncelleme İşlemi Burada Gerçekleşir.")
+})
+
 
 //Products
 app.use('/products',productsrouter);
